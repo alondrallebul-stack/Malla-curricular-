2d8ce3056221441410e2e9bc6fd32ef3fbfb0312
alondrallebul-stack <alondrallebul@gmail.com>
2025-12-10 18:26:16 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
+// Cargar ramos aprobados desde localStorage al iniciar
+document.addEventListener("DOMContentLoaded", () => {
+    const courses = document.querySelectorAll(".course");
 
+    courses.forEach(course => {
+        const id = course.dataset.id;
+
+        // Si estaba aprobado previamente
+        if (localStorage.getItem(id) === "aprobado") {
+            course.classList.add("approved");
+        }
+
+        // Evento al hacer clic
+        course.addEventListener("click", () => {
+            course.classList.toggle("approved");
+
+            if (course.classList.contains("approved")) {
+                localStorage.setItem(id, "aprobado");
+            } else {
+                localStorage.removeItem(id);
+            }
+        });
+    });
+});
